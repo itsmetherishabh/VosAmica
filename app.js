b421bb3c1debdc91f04a427e99faacb97b82ab34
ritikgarg829 <mycomputer8295@gmail.com>
2021-05-05 15:41:34 +0530

    footer
    footer

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const {check, validationResult } = require('express-validator');
-const {matchedData, sanitizeBody} = require('express-validator');
-const request=require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require('express-validator');
+const { matchedData, sanitizeBody } = require('express-validator');
+const request = require("request");
 const mongoose = require('mongoose');
 const ejs = require("ejs");
 
 
-const app=express();
+const app = express();
 
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-    res.render("index");
-  });
+app.get("/", function(req, res) {
+
+    res.render("footer");
+});
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
-  });
+});
