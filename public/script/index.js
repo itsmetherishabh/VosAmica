d0cfcef081a209bd98a1b5431d6571dd5cd1f884
divyagoyal48 <83334457+divyagoyal48@users.noreply.github.com>
2021-06-15 23:18:06 +0530

    modal

--- public/script/index.js
+++ public/script/index.js
@@ -31,9 +31,40 @@ window.addEventListener('keyup', (e) => {
 // --------------------------------------------------------
 
 
-function changeCity()
-{
-  var name=document.getElementById('city').value;
-  console.log(name);
-  alert(name);
+
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
 }
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
